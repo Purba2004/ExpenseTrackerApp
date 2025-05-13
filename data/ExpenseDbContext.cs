9447ab114c3f23307981f152bc3fefc003c83511
Purba Nandi <purba.nandi.r@gmail.com>
2025-05-14 03:34:07 +0530

    Add files via upload

--- data/ExpenseDbContext.cs
+++ data/ExpenseDbContext.cs
@@ -1,1 +1,14 @@
+using Microsoft.EntityFrameworkCore;
 
+using ExpenseTrackerApp.Models;
+
+namespace ExpenseTrackerApp.Data
+{
+    public class ExpenseDbContext : DbContext
+    {
+        public ExpenseDbContext(DbContextOptions<ExpenseDbContext> options)
+            : base(options) { }
+
+        public DbSet<Expense> Expenses { get; set; }
+    }
+}
